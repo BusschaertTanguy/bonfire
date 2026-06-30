83873b7f7724c5543c4446d4a7847e9a1f571c68
Tanguy Busschaert <tanguy.busschaert@devl-up.com>
2026-06-30 11:55:22 +0200

    Add react compiler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,6 +18,9 @@ export default defineConfig({
             autoCodeSplitting: true,
         }),
         react(),
+        babel({
+            presets: [reactCompilerPreset()],
+        }),
         tailwindcss(),
     ],
     build: {
